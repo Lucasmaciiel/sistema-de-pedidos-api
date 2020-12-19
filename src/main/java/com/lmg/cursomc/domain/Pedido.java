64a6eda8d2c38e08b0d503c8d339b1965e28fe11
Lucasmaciiel <lucasmacielgois@gmail.com>
2020-12-19 19:56:56 -0300

    Utilizando somente JsonIgnore

--- src/main/java/com/lmg/cursomc/domain/Pedido.java
+++ src/main/java/com/lmg/cursomc/domain/Pedido.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity
 public class Pedido implements Serializable {
 
@@ -27,10 +26,10 @@ public class Pedido implements Serializable {
 	private Integer id;
 	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date instante;
-	@JsonManagedReference
+	
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
-	@JsonManagedReference
+	
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
