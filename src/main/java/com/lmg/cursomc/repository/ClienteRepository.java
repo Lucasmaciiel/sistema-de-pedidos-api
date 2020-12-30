36668b5e40eadc2ef1b6cb4de937fe79e2fac4af
Lucasmaciiel <lucasmacielgois@gmail.com>
2020-12-30 17:01:24 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/lmg/cursomc/repository/ClienteRepository.java
+++ src/main/java/com/lmg/cursomc/repository/ClienteRepository.java
@@ -1,9 +1,13 @@
 package com.lmg.cursomc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lmg.cursomc.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
+	
 }
--- src/main/java/com/lmg/cursomc/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/lmg/cursomc/service/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.lmg.cursomc.domain.Cliente;
 import com.lmg.cursomc.domain.enums.TipoCliente;
 import com.lmg.cursomc.dto.ClienteNewDTO;
+import com.lmg.cursomc.repository.ClienteRepository;
 import com.lmg.cursomc.resources.exception.FieldMessage;
 import com.lmg.cursomc.service.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+		Cliente email = repo.findByEmail(objDto.getEmail());
+		
+		if (email != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
