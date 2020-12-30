31bf7a4b936079b3b918145c38982d078dbec5b7
Lucasmaciiel <lucasmacielgois@gmail.com>
2020-12-30 12:33:56 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/lmg/cursomc/service/ClienteService.java
+++ src/main/java/com/lmg/cursomc/service/ClienteService.java
@@ -58,7 +58,7 @@ public class ClienteService {
 		try {
 			repository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionadas");
 		}
 
 	}
