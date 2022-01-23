7432be24127839ab0232a6a735bca1f72c136352
lucasmaciiel <lucasmacielgois@gmail.com>
2022-01-23 19:17:55 -0300

    Corrige erro

--- src/main/java/com/lmg/cursomc/resources/exception/FieldMessage.java
+++ src/main/java/com/lmg/cursomc/resources/exception/FieldMessage.java
@@ -2,7 +2,7 @@ package com.lmg.cursomc.resources.exception;
 
 import java.io.Serializable;
 
-public class FieldMessage implements Serializable{
+public class FieldMessage implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
