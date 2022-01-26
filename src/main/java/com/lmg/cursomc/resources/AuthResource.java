e92b9347e4d0643ba9576572b80eb39fb979fd25
lucasmaciiel <lucasmacielgois@gmail.com>
2022-01-26 15:13:51 -0300

    Refresh token

---
+++ src/main/java/com/lmg/cursomc/resources/AuthResource.java
@@ -0,0 +1,28 @@
+package com.lmg.cursomc.resources;
+
+import com.lmg.cursomc.security.JWTUtil;
+import com.lmg.cursomc.security.UserSS;
+import com.lmg.cursomc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response){
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer "+ token);
+        return ResponseEntity.noContent().build();
+    }
+}
