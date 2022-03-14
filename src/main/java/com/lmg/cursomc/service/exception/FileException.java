7a87324257d432158406ed903acc5de76bf4f922
lucasmaciiel <lucasmacielgois@gmail.com>
2022-03-14 18:21:51 -0300

    Tratando excecoes adequadamente

---
+++ src/main/java/com/lmg/cursomc/service/exception/FileException.java
@@ -0,0 +1,17 @@
+package com.lmg.cursomc.service.exception;
+
+public class FileException extends RuntimeException {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FileException(String msg) {
+		super(msg);
+	}
+
+	public FileException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
